ce429bb9f75ab05ad7bfd51360991c52a02dff81
mcantero15 <mariana.a.cantero15@gmail.com>
2023-08-02 19:18:30 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mariana Cantero on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
