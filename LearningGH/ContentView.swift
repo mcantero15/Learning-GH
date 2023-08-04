fb53217bca42968a9df43582f819d217031e4652
mcantero15 <mariana.a.cantero15@gmail.com>
2023-08-03 22:34:21 -0400

    Add image

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Image(systemName: "figure.wave")
             Text("What's up, CodeCrew!")
         }
         .padding()
